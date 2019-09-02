31226d5864a0310f37c3dd671245726f88d6fe48
Inazuma110 <c011703534@edu.teu.ac.jp>
2019-09-03 07:36:27 +0900

    Regressionクラスを作った

--- lib/regression.cpp
+++ lib/regression.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <random>
+#include "matplotlibcpp.h"
 
 class CSV{
   public:
@@ -18,6 +20,7 @@ class CSV{
       std::istringstream stream(line);
       std::vector<std::string> elems;
       while(getline(stream, elem, spliter)){
+        // table.push_back({elem});
         elems.push_back(elem);
       }
       table.push_back(elems);
@@ -29,7 +32,43 @@ class CSV{
 
 };
 
+class Regression{
+  public:
+    long double theta0, theta1;
+    Regression(){
+      std::random_device rd;
+      std::mt19937_64 mt(rd());
+      std::uniform_real_distribution<double> theta(0.0, 1.0);
+      theta0 = theta(mt);
+      theta1 = theta(mt);
+    }
+
+
+
+
+
+};
+
+
+namespace plt = matplotlibcpp;
+
 int main(){
   CSV csv("./click.csv");
+  std::vector<double> x, y;
+
+  for (int i = 1; i < int(csv.table.size()); i++) {
+    // std::cout << csv.table[0][i] << std::endl;
+    x.push_back(stod(csv.table[i][0]));
+    y.push_back(stod(csv.table[i][1]));
+  }
+  // for (int i = 1; i < int(csv.table[0].size()); i++) {
+  //   y.push_back(stod(csv.table[1][i]));
+  //   // y[i] = 2 * i;
+  // }
+
+  Regression r;
+
+  // plt::scatter<double, double>(x, y, 50);
+  // plt::show();
   return 0;
 }
